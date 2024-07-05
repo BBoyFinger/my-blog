297d3e2603ee812c35789d457882ed3887dc7791
Vũ Ngọc Tú <80776665+tuvngch18505@users.noreply.github.com>
2024-07-05 22:43:01 +0700

    update api

--- src/app/api/category/index.post.ts
+++ src/app/api/category/index.post.ts
@@ -1,28 +1,28 @@
-import { db } from "@/lib/db";
-import { NextApiRequest, NextApiResponse } from "next";
-import { Prisma } from "@prisma/client";
+// import { db } from "@/lib/db";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { Prisma } from "@prisma/client";
 
-export default async function handler({
-  req,
-  res,
-}: {
-  req: NextApiRequest;
-  res: NextApiResponse;
-}) {
-  const { name, description } = req.body;
+// export default async function handler({
+//   req,
+//   res,
+// }: {
+//   req: NextApiRequest;
+//   res: NextApiResponse;
+// }) {
+//   const { name, description } = req.body;
 
-  try {
-    const category = await db.category.create({
-      data: {
-        name,
-        description,
-      },
-    });
-    res.status(200).json({
-      message: "Create category successfully!",
-      category: category,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
+//   try {
+//     const category = await db.category.create({
+//       data: {
+//         name,
+//         description,
+//       },
+//     });
+//     res.status(200).json({
+//       message: "Create category successfully!",
+//       category: category,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
